ec3970705c0a6afe16262fc08696f67018b94c34
Pramod <attanayakepramod@gmail.com>
2024-02-01 18:52:08 +0530

    first commit

---
+++ Weather_App/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { WeatherApp } from "./Components/WeatherApp/WeatherApp";
+import '../src/Components/WeatherApp/WeatherApp.css'
+function App(){
+  return(
+    <div>
+      <WeatherApp/>
+    </div>
+  )
+}
+export default App;
